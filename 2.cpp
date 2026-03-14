b8cf417dbde5a866e9e917861bc02216af573e17
Angelo <angelonunes269@gmail.com>
2026-03-14 14:09:26 -0300

    Update 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main()
+{
+double numero1 ;
+ 
+ cout << "digite o numero";
+ cin >> numero1;
+ 
+cout << fixed << setprecision(3);
+cout << "o numero é " << numero1 <<endl;
+ 
+ return 0;
+}

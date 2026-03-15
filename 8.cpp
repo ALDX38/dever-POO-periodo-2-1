5102a6a6f02edf7516cfbcc04353180821c032cb
Angelo <angelonunes269@gmail.com>
2026-03-15 20:21:24 -0300

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,16 @@
+]#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int numero;
+    
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+    
+    cout << "Divisao inteira por 2: " << numero / 2 << endl;
+    cout << "Divisao real (ponto flutuante) por 2: " << numero / 2.0 << endl;
+    
+    return 0;
+}
